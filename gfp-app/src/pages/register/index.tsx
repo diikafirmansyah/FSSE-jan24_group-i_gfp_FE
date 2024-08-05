1812000c17ffc081777fea98350f54c24dadce36
Hari <155520906+ImamHM04@users.noreply.github.com>
2024-08-06 01:37:35 +0700

    remove dark mode

--- gfp-app/src/pages/register/index.tsx
+++ gfp-app/src/pages/register/index.tsx
@@ -30,21 +30,21 @@ const Register = () => {
 
   const router = useRouter();
 
-//   const onChangeUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     setuserName(event.target.value);
-//   };
+  const onChangeUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setuserName(event.target.value);
+  };
 
-//   const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     setEmail(event.target.value);
-//   };
+  const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
 
-//   const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     setPassword(event.target.value);
-//   };
+  const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
 
-// 	const onChangePhoneNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
-// 		setPhoneNumber(event.target.value);
-// 	};
+	const onChangePhoneNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setPhoneNumber(event.target.value);
+	};
 
   const registerSchema = Yup.object().shape({
     username: Yup.string().required('*Name is required').min(3, '*Name must be at least 3 characters!'),
@@ -53,14 +53,22 @@ const Register = () => {
 		phoneNumber: Yup.string().required('*Phone number is required')
   });
 
-  const handleRegister = async (values: {username: string, email: string, password: string, phoneNumber: string}) => {
-		const response = await fetch('http://localhost:3000/users/register,', {
+  const handleRegister = async (username: string, email: string, password: string, phoneNumber: string) => {
+
+		console.log('Register', username, email, password, phoneNumber);
+
+		const formData = new URLSearchParams();
+    formData.append("email", email);
+    formData.append("password", password);
+		formData.append("username", username);
+		formData.append("phoneNumber", phoneNumber);
+
+		const response = await fetch('http://127.0.0.1:5000/users/register,', {
 			method: 'POST',
 			headers: {
-				'Content-Type': 'application/json',
+				'Content-Type': 'application/x-www-form-urlencoded',
 			},
-			body: JSON.stringify({ 
-				name: values.username, email: values.email, password: values.password, phone: values.phoneNumber }),
+			body: formData.toString(),
 		})
 		console.log('Response register', response);
     const result = await response.json()
@@ -81,7 +89,7 @@ const Register = () => {
 
   return (
     <section className='relative min-h-screen items-center justify-center'>
-			<div className="absolute inset-0 bg-gray-900">
+			<div className="absolute inset-0">
         <img
           src="./assets/bg-register.jpg"
           alt="Background"
@@ -90,59 +98,53 @@ const Register = () => {
 			</div>
       <div className="relative z-10">
 				<div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 drop-shadow-2xl">
-					<div className="md:flex-wrap w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-2xl xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+					<div className="md:flex-wrap w-full rounded-lg shadow border md:mt-0 sm:max-w-2xl xl:p-0 bg-white border-white">
 						<div className="p-6 space-y-4 md:space-y-6 sm:p-8">
 							<div className="w-40 h-20 mx-auto">
 								<img src="./assets/logo.png" alt="logo"/>
 							</div>
-							<h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">Register Here</h1>
+							<h1 className="text-xl font-bold leading-tight tracking-tight  md:text-2xl text-gray-700 text-center">Register Here</h1>
 							<Formik
 								initialValues={initialValues}
-								validationSchema={registerSchema}
-								onSubmit={(values) => {
-									console.log('register', {
-										username: values.username,
-										email: values.email,
-										password: values.password,
-										phoneNumber: values.phoneNumber
-									})
-									const data = {
-										username: values.username,
-										email: values.email,
-										password: values.password,
-										phoneNumber: values.phoneNumber
-									}
-									if (values.username && values.email && values.password && values.phoneNumber) handleRegister(data)
-								}}>
+                validationSchema={registerSchema}
+                onSubmit={(values) => {
+                  handleRegister(
+										values.username, 
+										values.email, 
+										values.password,
+										values.phoneNumber	
+									);
+                }}
+								>
 								<Form action="#" className="space-y-4 md:space-y-6">
 									<div className='flex flex-row justify-between gap-[2.5em]'>
 										<div className="w-1/2">
 											<div className='flex gap-2'>
-												<FaUser className='mt-[2px]'/>
-												<label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+												<FaUser className='mt-[2px] icon-black'/>
+												<label htmlFor="name" className="block mb-2 text-sm font-medium  text-gray-900">Username</label>
 											</div>
-											<input
-												id = "name"
-												name="name"
+											<Field
+												id = "username"
+												name="username"
 												type="text"
 												placeholder="your name"
-												onChange={(e:any) => setuserName(e.target.value)}
-												className="border border-blue-500 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:ring-blue-500 focus:border-blue-500"
+												values={onChangeUserName}
+												className="border text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 border-gray-600 placeholder-gray-400 text-white focus:border-blue-500"
 											/>
 											<ErrorMessage name='username' component='div' className='text-red-600 text-sm'/>
 										</div>
 										<div className="w-1/2">
 											<div className='flex gap-2 '>
-												<IoIosMail className='mt-[3px]'/>
-												<label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
+												<IoIosMail className='mt-[3px] icon-black'/>
+												<label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email address</label>
 											</div>
-											<input
+											<Field
 												id = "email"
 												name="email"
 												type="email"
 												placeholder="example123@ex.com"
-												onChange={(e:any) => setEmail(e.target.value)}
-												className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+												values={onChangeEmail}
+												className="border text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  border-gray-600 placeholder-gray-400 text-white focus:ring-border-500 focus:border-blue-500"
 											/>
 											<ErrorMessage name='email' component='div' className='text-red-600 text-sm'/>
 										</div>
@@ -151,42 +153,42 @@ const Register = () => {
 									<div className='flex flex-row justify-between gap-[2.5em]'>
 										<div className="w-1/2">
 												<div className='flex gap-2'>
-													<BsPhone className='mt-[2px]'/>
-													<label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone</label>
+													<BsPhone className='mt-[2px] icon-black'/>
+													<label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Phone</label>
 												</div>
-												<input
-													id = "name"
-													name="name"
+												<Field
+													id = "phoneNumber"
+													name="phoneNumber"
 													type="number"
 													placeholder="your phone number"
-													onChange={(e:any) => setPhoneNumber(e.target.value)}
-													className="border border-blue-500 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:ring-blue-500 focus:border-blue-500"
+													values={onChangePhoneNumber}
+													className="border text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  border-gray-600 placeholder-gray-400 text-gray-900 focus:border-blue-500"
 												/>
 												<ErrorMessage name='phoneNumber' component='div' className='text-red-600 text-sm'/>
 												</div>
 										<div className="w-1/2">
 											<div className='flex gap-2 '>
-												<GiPadlock className='mt-[3px]'/>
-												<label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+												<GiPadlock className='mt-[3px] icon-black'/>
+												<label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
 											</div>
-											<div className = "flex justify-between items-center p-2.5 rounded-lg space-x-1 border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-												<input
+											<div className = "flex justify-between items-center p-2.5 rounded-lg space-x-1 border border-gray-600 placeholder-gray-400 text-white focus:ring-border-500 focus:border-blue-500">
+												<Field
 													id = "password"
 													name="password"
-													onChange={(e:any) => setPassword(e.target.value)}
+													values={onChangePassword}
 													type={visible ? 'password' : 'text'}
 													placeholder="**********"
-													className="block w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+													className="block w-full  placeholder-gray-400 text-gray-900"
 												/>
 												<div className='cursor-pointer' onClick={() => setVisible(!visible)}>
-													{visible?  <FaEyeSlash /> : <IoEyeSharp />}
+													{visible?  <FaEyeSlash className='icon-black'/> : <IoEyeSharp className='icon-black'/>}
 												</div>
 											</div>
 											<ErrorMessage name='password' component='div' className='text-red-600 text-sm'/>
 										</div>	
 									</div>	
 									<div className='flex gap-10 justify-center'>
-										<h4 className="items-center mt-[13px] font-bold">Role:</h4>
+										<h4 className="items-center mt-[13px] font-bold text-gray-900">Role:</h4>
 										<div className="flex justify-center gap-[3em] cursor-pointer">
 											<div className="flex items-center  ">
 												<input 
@@ -194,17 +196,17 @@ const Register = () => {
 													type="radio"
 													value=" "
 													name="bordered-radio"
-													className="w-4 h-4 cursor-pointer text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-												<label htmlFor='bordered-1' className='w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300'>Seller</label>
+													className="w-4 h-4 cursor-pointer ring-offset-gray-800 focus:ring-2 bg-gray-700 border-gray-600" />
+												<label htmlFor='bordered-1' className='w-full py-4 ms-2 text-sm font-medium text-gray-900'>Seller</label>
 											</div>
 											<div className="flex items-center ">
-												<input
+												<Field
 													id="bordered-1" 
 													type="radio"
 													value=" "
 													name="bordered-radio"
-													className="w-4 h-4 cursor-pointer text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-												<label htmlFor='bordered-1' className='w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300'>Customer</label>
+													className="w-4 h-4 cursor-pointer text-blue-600  ring-offset-gray-800 focus:ring-2 bg-gray-700 border-gray-600" />
+												<label htmlFor='bordered-1' className='w-full py-4 ms-2 text-sm font-medium text-gray-900'>Customer</label>
 											</div>	
 										</div>	
 									</div>
@@ -214,15 +216,15 @@ const Register = () => {
 												id="terms" 
 												aria-describedby="terms" 
 												type="checkbox" 
-												className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+												className="w-4 h-4 border rounded focus:ring-3 focus:ring-primary-300 bg-gray-700 border-gray-600 focus:ring-primary-600 ring-offset-gray-800"
 											/>
 										</div>
 										<div className="ml-3 text-sm">
-											<label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline text-blue-600 ">Terms and Conditions</a></label>
+											<label htmlFor="terms" className="text-black">I accept the <a className="font-medium text-primary-600 hover:underline text-blue-600 ">Terms and Conditions</a></label>
 										</div>
 									</div>
-									<button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 bg-blue-600 hover:bg-blue-800 transition duration-300">Create an account</button>
-									<p>Already have an account? <a onClick={() => router.push('/login')} className="text-primary-600 hover:underline text-blue-600 cursor-pointer">Login here</a></p>
+									<button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800 bg-blue-600 hover:bg-blue-800 transition duration-300">Create an account</button>
+									<p className="text-black">Already have an account? <a onClick={() => router.push('/login')} className="text-primary-600 hover:underline text-blue-600 cursor-pointer">Login here</a></p>
 								</Form>
 							</Formik>
 						</div>
