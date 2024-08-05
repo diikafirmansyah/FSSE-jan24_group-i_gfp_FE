a13eb334e93332d79aa1ef3b0ff47b683ec93a55
FrederickAlex17 <frederick.alexander@student.umn.ac.id>
2024-08-05 19:47:44 +0700

    footer added

--- gfp-app/src/pages/_app.tsx
+++ gfp-app/src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Footer from '@/pages/footer';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import '@/lib/fontawesome'; // Adjust the path as needed
+
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </div>
+  );
 }
