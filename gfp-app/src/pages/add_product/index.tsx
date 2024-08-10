4b72ddaa3f0f99fec5271f0a3523858ddb946a7d
dika firmansyah <diikafirmansyah94@gmail.com>
2024-08-11 01:26:28 +0900

    chore : fix styling

--- gfp-app/src/pages/add_product/index.tsx
+++ gfp-app/src/pages/add_product/index.tsx
@@ -4,8 +4,8 @@ import AddProductForm from '@/components/AddProductForm';
 
 const AddProductPage: NextPage = () => {
   return (
-    <div>
-      <h1>Add Product</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-black p-6">
+      <h1 className='text-3xl font-bold'>Add Product</h1>
       <AddProductForm />
     </div>
   );
