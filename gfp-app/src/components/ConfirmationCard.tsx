b192f080d9f73748a91fc5419db24658d13fd457
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-08-15 20:10:21 +0700

    feat: add confirmations logic to use data from database

--- gfp-app/src/components/ConfirmationCard.tsx
+++ gfp-app/src/components/ConfirmationCard.tsx
@@ -1,32 +1,82 @@
 // components/ConfirmationCard.tsx
-import React from 'react';
+import { API_URL } from '@/config';
+import React, { useState } from 'react';
 
-interface Product {
-  name: string;
-  price: number;
-}
-
-interface ConfirmationCardProps {
-  product: Product;
+interface Confirmation {
+  id: number;
+  user_id: number;
   buyer: string;
+  product_id: number;
+  price: number;
   qty: number;
-  onConfirm: () => void;
+  description: string;
+  created_at: string;
+  updated_at: string | null;
+  is_confirm: number;
 }
 
-const ConfirmationCard: React.FC<ConfirmationCardProps> = ({ product, buyer, qty, onConfirm }) => {
+const ConfirmationCard: React.FC<Confirmation> = ({
+  id,
+  user_id,
+  buyer,
+  product_id,
+  price,
+  qty,
+  description,
+  created_at,
+  updated_at,
+  is_confirm
+}) => {
+
+  const [isConfirmed, setIsConfirmed] = useState<number>(is_confirm);
+
+  const onConfirm = async () => {
+    try {
+      const token = localStorage.getItem("access_token");
+      const response = await fetch(`${API_URL}/confirmations/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: "Bearer " + token,
+        }
+      });
+
+      if (!response) {
+        alert("Confirmation failed!");
+        return;
+      }
+
+      const result = await response;
+      console.log("Transaction confirmed!", result);
+      alert("Transaction confirmed!");
+      window.location.reload();
+    } catch (error) {
+      console.error("Error submitting the form", error);
+      alert("An error occurred while confirming the transaction");
+    }
+  };
+
   return (
     <div className="border border-gray-300 p-6 rounded-lg shadow-lg bg-white">
       <h2 className="text-2xl font-bold mb-4">Confirmation</h2>
-      <p className="mb-2"><strong className="font-medium">Product:</strong> {product.name}</p>
-      <p className="mb-2"><strong className="font-medium">Price:</strong> ${product.price.toFixed(2)}</p>
-      <p className="mb-2"><strong className="font-medium">Quantity:</strong> {qty}</p>
-      <p className="mb-4"><strong className="font-medium">Total:</strong> ${ (product.price * qty).toFixed(2) }</p>
+      <p className="mb-2"><strong className="font-medium">Product:</strong> {description} </p>
+      <p className="mb-2"><strong className="font-medium">Quantity:</strong> {qty} </p>
+      <p className="mb-4"><strong className="font-medium">Total Price: </strong> 
+         {new Intl.NumberFormat("id-ID", {
+          style: "currency",
+          currency: "IDR",
+        }).format(price)} </p>
       <p className="mb-4"><strong className="font-medium">Buyer:</strong> {buyer}</p>
-      <button 
-        onClick={onConfirm} 
-        className="px-4 py-2 bg-green-600 text-white font-semibold rounded-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500"
+      <button
+        onClick={onConfirm}
+        disabled={isConfirmed ? true : false}
+        className={`px-4 py-2 text-white font-semibold rounded-lg focus:outline-none focus:ring-2 ${
+          isConfirmed ? 
+          `bg-gray-400 cursor-not-allowed`
+          : `bg-green-600 hover:bg-green-700 focus:ring-green-500`
+        }`}
       >
-        Confirm Purchase
+        {isConfirmed ? 'Confirmed' : 'Confirm Purchase'}
       </button>
     </div>
   );
