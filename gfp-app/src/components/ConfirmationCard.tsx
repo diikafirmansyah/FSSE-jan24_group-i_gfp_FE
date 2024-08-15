507c62f13a1da34eabd05f869d319c318d78c9c8
Raden Wisnu Andhika Pranidhia <radenwisnu21@gmail.com>
2024-08-15 20:50:28 +0700

    feat: completing confirmation card and function

--- gfp-app/src/components/ConfirmationCard.tsx
+++ gfp-app/src/components/ConfirmationCard.tsx
@@ -28,8 +28,6 @@ const ConfirmationCard: React.FC<Confirmation> = ({
   is_confirm
 }) => {
 
-  const [isConfirmed, setIsConfirmed] = useState<number>(is_confirm);
-
   const onConfirm = async () => {
     try {
       const token = localStorage.getItem("access_token");
@@ -41,7 +39,7 @@ const ConfirmationCard: React.FC<Confirmation> = ({
         }
       });
 
-      if (!response) {
+      if (!response.ok) {
         alert("Confirmation failed!");
         return;
       }
@@ -61,6 +59,7 @@ const ConfirmationCard: React.FC<Confirmation> = ({
       <h2 className="text-2xl font-bold mb-4">Confirmation</h2>
       <p className="mb-2"><strong className="font-medium">Product:</strong> {description} </p>
       <p className="mb-2"><strong className="font-medium">Quantity:</strong> {qty} </p>
+      <p className="mb-2"><strong className="font-medium">Date:</strong> {created_at} </p>
       <p className="mb-4"><strong className="font-medium">Total Price: </strong> 
          {new Intl.NumberFormat("id-ID", {
           style: "currency",
@@ -69,14 +68,14 @@ const ConfirmationCard: React.FC<Confirmation> = ({
       <p className="mb-4"><strong className="font-medium">Buyer:</strong> {buyer}</p>
       <button
         onClick={onConfirm}
-        disabled={isConfirmed ? true : false}
+        disabled={is_confirm ? true : false}
         className={`px-4 py-2 text-white font-semibold rounded-lg focus:outline-none focus:ring-2 ${
-          isConfirmed ? 
+          is_confirm ? 
           `bg-gray-400 cursor-not-allowed`
           : `bg-green-600 hover:bg-green-700 focus:ring-green-500`
         }`}
       >
-        {isConfirmed ? 'Confirmed' : 'Confirm Purchase'}
+        {is_confirm ? 'Confirmed' : 'Confirm Purchase'}
       </button>
     </div>
   );
--- gfp-app/src/pages/confirm/index.tsx
+++ gfp-app/src/pages/confirm/index.tsx
@@ -1,33 +1,99 @@
 // pages/confirm.tsx
 import { NextPage } from 'next';
 import ConfirmationCard from '@/components/ConfirmationCard';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { API_URL } from '@/config';
+import useAuth from '@/middleware/auth';
+import Loading from '@/components/Loading';
+
+interface Confirmation {
+  id: number;
+  user_id: number;
+  buyer: string;
+  product_id: number;
+  price: number;
+  qty: number;
+  description: string;
+  created_at: string;
+  updated_at: string | null;
+  is_confirm: number;
+}
 
 const ConfirmPage: NextPage = () => {
-  // Contoh data yang bisa diambil dari API atau state global
+  useAuth();
 
-  const product = {
-    name: 'Sample Product',
-    price: 49.99
-  };
-  const buyer = 'John Doe';
-  const qty = 2;
+  const [confirmations, setConfirmations] = useState<Confirmation[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<any>(null);
 
-  const handleConfirm = () => {
-    
-    alert('Purchase confirmed!');
-  };
+  useEffect(() => {
+    const fetchConfirmations = async () => {
+      const token = localStorage.getItem('access_token');
+      try {
+        const response = await fetch(`${API_URL}/confirmations/me`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Bearer " + token
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        setConfirmations(result.confirmations || []);
+        // console.log(result.confirmations);
+      } catch (error) {
+        console.error("Error fetching confirmations:", error);
+        setError(error)
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchConfirmations();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="max-w-5xl mx-auto py-12 px-4 flex justify-center items-center h-[calc(100vh-6rem)]">
+        <Loading />
+      </div>
+    );
+  }
+
+  if (error) return (
+    <div className="max-w-5xl mx-auto py-12 px-4 flex justify-center items-center h-[calc(100vh-6rem)]">
+      {error}
+    </div>
+  );
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <ConfirmationCard
-        product={product}
-        buyer={buyer}
-        qty={qty}
-        onConfirm={handleConfirm}
-      />
+    <div className="flex justify-center items-center h-screen flex-wrap gap-2">
+      {confirmations.length > 0 ? (
+        confirmations.map((item) => (
+          <ConfirmationCard
+            key={item.id}
+            id={item.id}
+            user_id={item.user_id}
+            buyer={item.buyer}
+            product_id={item.product_id}
+            price={item.price}
+            qty={item.qty}
+            description={item.description}
+            created_at={item.created_at}
+            updated_at={item.updated_at}
+            is_confirm={item.is_confirm}
+          />
+        ))
+      ) : (
+        <p className="col-span-full text-center text-gray-500">
+          No confirmations
+        </p>
+      )}
     </div>
-    
   );
 };
 
